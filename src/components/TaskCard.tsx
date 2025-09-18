dfd758c3f9c046c81cd4f7cf0ca9709697f33db2
LindaKat <lindakt16@gmail.com>
2025-09-18 17:02:50 -0500

    more style cleanup, plus had to refresh lock file for vite

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,6 +1,5 @@
 import styles from "@styles/task.module.css";
 import { type Task } from "@utils/types.ts";
-import { Icon } from "astro-icon/components";
 import { actions } from "astro:actions";
 import { useRef, useState } from "react";
 
@@ -16,6 +15,7 @@ export default function TaskCard({
   const detailsDialogRef = useRef<HTMLDialogElement | null>(null);
   const completionDialogRef = useRef<HTMLDialogElement | null>(null);
   const [completedDescription, setCompletedDescription] = useState("");
+  const [taskComplete, setTaskComplete] = useState(task.taskComplete);
 
   const allowCompletion =
     task.taskAction.task !== "Do the application" ? true : taskPrereq;
@@ -24,14 +24,18 @@ export default function TaskCard({
     if (detailsDialogRef.current) detailsDialogRef.current.showModal();
   };
 
-  const closeDialog = () => {
+  const closeDetailsDialog = () => {
     if (detailsDialogRef.current) detailsDialogRef.current.close();
-    if (completionDialogRef.current) completionDialogRef.current.close();
   };
 
   const openCompleteDialog = () => {
     if (detailsDialogRef.current) detailsDialogRef.current.close();
     if (completionDialogRef.current) completionDialogRef.current.showModal();
+  };
+
+  const closeCompleteDialog = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (completionDialogRef.current) completionDialogRef.current.close();
   };
 
   const handleCompletion = async (e: React.FormEvent) => {
@@ -48,44 +52,52 @@ export default function TaskCard({
       journeyId: journeyId,
     });
 
-    if (!error && completionDialogRef.current)
-      completionDialogRef.current.close();
+    if (!error) {
+      setTaskComplete(true);
+      if (completionDialogRef.current) completionDialogRef.current.close();
+    } else {
+      console.log({ error });
+    }
   };
 
   return (
     <>
       <div className={styles.taskWrapper}>
-        {!task.taskComplete ? (
+        {!taskComplete ? (
           <button className={styles.openBtn} onClick={openDetailsDialog}>
             <p>{task.taskId}</p>
             <p>{task.taskAction.task}</p>
           </button>
         ) : (
-          <Icon name="star-filled" class={styles.taskComplete} />
+          <div className={styles.completed}>
+            <p>Done</p>
+          </div>
         )}
       </div>
 
       <dialog ref={detailsDialogRef}>
-        <button className={styles.closeBtn} onClick={closeDialog}>
-          Close
-        </button>
-        <p>{task.taskAction.tip}</p>
-        <button
-          className={styles.doneBtn}
-          onClick={openCompleteDialog}
-          disabled={allowCompletion}
-        >
-          Mark as Completed!
-        </button>
+        <div>
+          <button className={styles.closeBtn} onClick={closeDetailsDialog}>
+            Close
+          </button>
+          <p>{task.taskAction.tip}</p>
+          <button
+            className={styles.doneBtn}
+            onClick={openCompleteDialog}
+            disabled={!allowCompletion}
+          >
+            Completed!
+          </button>
+        </div>
       </dialog>
 
       <dialog ref={completionDialogRef}>
-        <form className={styles.completeForm}>
-          <button className={styles.closeBtn} onClick={closeDialog}>
+        <form>
+          <button className={styles.closeBtn} onClick={closeCompleteDialog}>
             Close
           </button>
           <label htmlFor="textarea">
-            Leave a note for yourself of what you did to complete this task:
+            Write a reminder for future you on how you completed this task:
           </label>
           <textarea
             id="textarea"
--- src/utils/taskList.ts
+++ src/utils/taskList.ts
@@ -1,10 +1,10 @@
 export const taskList = [
   {
-    task: "Send a message to 2 people in your contact list",
+    task: "Connect with 2 current contacts",
     tip: "Keep your connections warm! Find 2 people you haven't talked with in a little while and see how they're doing, let them know you're thinking of them, or support them with something they're sharing online.",
   },
   {
-    task: "5 minute intro practice",
+    task: "Practice intro for 5 minutes",
     tip: "'Tell me about yourself' will come up all the time. Spend 5 minutes practicing speaking yours out loud and/or deciding what you want to say when people ask.",
   },
   {
@@ -20,11 +20,11 @@ export const taskList = [
     tip: "Now that you've got a role to apply for, it's time to learn more about this company. Check their website for an about or values page to see what's important to them. Check for social media pages and see what they're sharing. Take notes on anything that comes up often, especially if you can see how it aligns with skills and/or values you have.",
   },
   {
-    task: "1 new LinkedIn connection",
+    task: "Add 1 LinkedIn connection",
     tip: "Send a personalized connection request to one new person. If possible, find someone at the company you're vetting this week and connect with them. Or you can find someone with a role you'd like to have one day, or whose posts you've been vibing with. Send them a note with your connection request to share what drew you to reach out - don't make it about the job you're applying for, but something about what seems interesting for what they're posting or the job they hold. Find a common interest you have and include that.",
   },
   {
-    task: "3 ways you're a great match",
+    task: "List 3 ways you're the best pick",
     tip: "Look through the job description and find 3 specific points that match with your experience. Write a sentence for each point that highlights how your skills match it - create a story you can share with them on what makes you a great fit for this role. Make sure to check what the application will require too - sometimes there's specific questions they want you to fill out, this is a great time to find your answers for those.",
   },
   {
@@ -32,7 +32,7 @@ export const taskList = [
     tip: "Time to send in your application! Use the work you've done on researching the company and finding your fit and make sure to highlight those points when filling out the application. If they don't have any specific questions in the application to explain how awesome you'd be, make sure to include a cover letter detailing those points for them. NOTE: this is the only task with a prerequisite! You must have completed the 'Find a job to apply for' task first in order to complete this.",
   },
   {
-    task: "LinkedIn post - role fit",
+    task: "LinkedIn post: role fit",
     tip: "Write a LinkedIn post sharing one of your top skills that make you great at the types of roles you're applying to. You've done some research for one of these roles already, so you can use one of those points to tell the story! Or you can share a time you got to use one of your more core skills. Make this something that, when the company you just applied to clicks your profile, they'll immediately see a value you bring that lines up for them.",
   },
   {
@@ -44,7 +44,7 @@ export const taskList = [
     tip: "Gotta keep those skills sharp! Do a short, bite sized coding problem. This could be something like LeetCode or a tightly scoped design problem. Something like you might see in a coding interview that can be completed within an hour.",
   },
   {
-    task: "LinkedIn post - skill share",
+    task: "LinkedIn post: skill share",
     tip: "Share something you learned from the event/video you saw or the code problem you solved. Showcase that you're actively working on keeping your skills sharp, learning new ones, and staying active within your community however you can.",
   },
   {
@@ -52,7 +52,7 @@ export const taskList = [
     tip: "If it's on your resume, it's free game to be asked about in an interview. Pick one bullet point and practice talking about it. Using the STAR method is great for this - tell the story of this point by talking through the situation, the task you were given, what action you took to solve it, and the result you got. Share what makes this worth being on your resume and the value you bring. Practice saying it out loud if you've already go the storyline written.",
   },
   {
-    task: "Leave 3 thoughtful comments",
+    task: "Leave 3 detailed comments",
     tip: "Check your LinkedIn feed for the week and leave 3 comments on others posts. Make sure the comment you're leaving is meaningful - don't just say 'nice job', point out what you love about it, something it makes you think of, a question you have for how they worded it. Add some value and show that you're actually paying attention to what they wrote.",
   },
   {
