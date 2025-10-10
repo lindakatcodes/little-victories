25a1464e098a1d86c5384f8566032f73f81ad229
LindaKat <lindakt16@gmail.com>
2025-10-10 15:03:57 -0500

    setup head tags; create nav component and add to main layout; adjust related styles

--- app/app.vue
+++ app/app.vue
@@ -1,13 +1,16 @@
 <template>
   <main>
+    <NavBar />
     <NuxtPage />
   </main>
 </template>
 
 <style>
+/* global stylesheet is setting css vars and default text sizing */
 @import url("~/assets/styles/global.css");
 
-:root {
+/* main layout settings */
+body {
   font-family: "Plus Jakarta Sans Variable", sans-serif;
   background: var(--light-blue, #bfdbf7);
   color: var(--dark-blue, #071f36);
@@ -18,5 +21,24 @@
 main {
   container: main / inline-size;
   inline-size: 100%;
+}
+
+/* shared styles throughout the app */
+.styled-link {
+  padding: 0.5rem 0.75rem;
+  background-color: var(--dark-blue);
+  color: var(--light-blue);
+  border-radius: 0.25rem;
+  font-weight: 500;
+  text-decoration: none;
+}
+
+.inverse-styled-link {
+  padding: 0.5rem 0.75rem;
+  background-color: var(--light-blue);
+  color: var(--dark-blue);
+  border-radius: 0.25rem;
+  font-weight: 500;
+  text-decoration: none;
 }
 </style>
