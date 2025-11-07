5009377fc6b7dc5b630527c92ef7970b7c56eb71
LindaKat <lindakt16@gmail.com>
2025-11-06 18:01:26 -0600

    image changes worked so removing package no longer being used

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/image", "@pinia/nuxt", "nuxt-svgo", "@vueuse/nuxt"],
+  modules: ["@pinia/nuxt", "nuxt-svgo", "@vueuse/nuxt"],
   app: {
     head: {
       title: "Little Victories",
